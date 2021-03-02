25d6cfc6ca8c8b342a7acc9b3b4d5ac948af5f30
Kauthar Omar <kauthyomar@gmail.com>
2021-03-02 08:00:29 +0300

    greetings.sh

---
+++ greetings.sh
@@ -0,0 +1,20 @@
+
+echo " Please input your name"
+read name
+
+date=$(date +%H)
+time=$(date +%T)
+day=$(date +%D)
+
+if [ $date -lt "12" ]
+then
+echo "Good Morning ${name}"
+echo "It's now $time on this lovely day of $day."
+elif [[ $date -gt "12" && $date -lt "16" ]]
+then
+echo "Good Afternoon ${name}"
+echo "It's now $time on this lovely day of $day."
+else
+echo "Good Evening ${name}"
+echo "It's now $time on this lovely day of $day."
+fi
